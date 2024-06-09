50ac348f9aa3e1e7de4d967838d2b40b34a8e905
omataclas <omataclas@gmail.com>
2024-06-09 13:13:58 -0300

    new component + basic navbar

--- Angular-Bootsrap-LandingPageADS/angular-bootstrap-ADS/src/app/app.component.ts
+++ Angular-Bootsrap-LandingPageADS/angular-bootstrap-ADS/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
 import { ForumComponent } from './forum/forum.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, ForumComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent, ConteudoComponent, ForumComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
