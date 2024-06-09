35a06771c8940a9ddd0c6cf3de260b8cb1c9ba23
omataclas <omataclas@gmail.com>
2024-06-09 12:51:23 -0300

    Created 3 base components

--- Angular-Bootsrap-LandingPageADS/angular-bootstrap-ADS/src/app/app.component.ts
+++ Angular-Bootsrap-LandingPageADS/angular-bootstrap-ADS/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ForumComponent } from './forum/forum.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, ForumComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
