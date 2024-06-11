04463d2a2b627d80bdfaf7312630b63fb38e852a
omataclas <omataclas@gmail.com>
2024-06-10 22:22:59 -0300

    removed tets & integraded new components

--- Angular-Bootsrap-Lojinha/lojinha/src/app/app.component.ts
+++ Angular-Bootsrap-Lojinha/lojinha/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ProductsComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
